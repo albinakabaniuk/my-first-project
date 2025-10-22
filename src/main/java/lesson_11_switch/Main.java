c85c04dde4ffbcc23a0525a4fe4f3c48b001c182
Albina Kabanjuk <albinakabaniuk@gmail.com>
2025-10-22 19:45:35 +0300

    Додано switch java 12 для днів тижня

--- src/main/java/lesson_11_switch/Main.java
+++ src/main/java/lesson_11_switch/Main.java
@@ -53,5 +53,16 @@ public class Main {
             default:
                 System.out.println("Такого значення не існує");
         }
+
+        switch (number) {
+            case 1 -> System.out.println("Понеділок");
+            case 2 -> System.out.println("Вівторок");
+            case 3 -> System.out.println("Середа");
+            case 4 -> System.out.println("Четвер");
+            case 5 -> System.out.println("П'ятниця");
+            case 6 -> System.out.println("Субота");
+            case 7 -> System.out.println("Неділя");
+            default -> System.out.println("Такого значення не існує");
+        }
     }
 }
