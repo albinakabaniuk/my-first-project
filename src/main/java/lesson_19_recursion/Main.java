f26b3eb816e3c8c8b70d1785eb4eb648b196d404
Albina Kabanjuk <albinakabaniuk@gmail.com>
2025-11-03 17:58:34 +0200

    Виправлено на маленьку букву

--- src/main/java/lesson_19_recursion/Main.java
+++ src/main/java/lesson_19_recursion/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int Fibonacci(int n) {
+    public static int fibonacci(int n) {
         if (n == 0)
             return 0;
         if (n == 1 || n == 2)
             return 1;
 
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class Main {
         System.out.print("Введіть номер числа Фібоначчі: ");
         int n = scanner.nextInt();
 
-        int result = Fibonacci(n);
+        int result = fibonacci(n);
 
         System.out.println("Число Фібоначчі з номером " + n + " = " + result);
 
