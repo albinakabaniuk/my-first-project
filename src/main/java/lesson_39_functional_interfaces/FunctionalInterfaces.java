9b8cd7d23598a69049beee4419d7cd58261b124b
albinakabaniuk <albinakabaniuk@gmail.com>
2025-12-25 17:12:33 +0200

    Update src/main/java/lesson_39_functional_interfaces/FunctionalInterfaces.java
    Co-authored-by: Mykola Digtiar <mykola.digtiar@gmail.com>

--- src/main/java/lesson_39_functional_interfaces/FunctionalInterfaces.java
+++ src/main/java/lesson_39_functional_interfaces/FunctionalInterfaces.java
@@ -48,7 +48,9 @@ public class FunctionalInterfaces {
 
     public static UnaryOperator<Integer> factorial = n -> {
         int result = 1;
-        for (int i = 1; i <= n; i++) result *= i;
+        for (int i = 1; i <= n; i++) {
+           result *= i;
+        }
         return result;
     };
 
